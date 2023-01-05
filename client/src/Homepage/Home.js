8e29087fde2064dda1c14423dd9dffd2d8abeb06
mubasshir29 <mubasshir.f29>
2023-01-05 22:32:32 +0400

    final commit

--- client/src/Homepage/Home.js
+++ client/src/Homepage/Home.js
@@ -1,9 +1,99 @@
 import React from 'react'
 import './Home.css'
+import dashboard from './../SampleData/DashboardCardsData.json'
+import sw_vendors from './../SampleData/DashboardSwitchVendors.json'
+import fw_vendors from './../SampleData/DashboardFirewallVendors.json'
+import controller_vendors from './../SampleData/DashboardControllerVendors.json'
+import ap_vendors from './../SampleData/DashboardAPVendors.json'
+import Colors from '../SampleData/ColorList'
 
 function Home() {
   return (
-    <div className='homepage_content'>Home Page</div>
+    <div className='homepage_content'>
+        <div className='title'>
+          Dashboard
+        </div>
+
+        <div className='dashboard_title'>
+          <h6>Devices</h6>
+        </div>
+        
+        <div className='cards_container'>
+                <div className='dashboard_card' style={{backgroundColor:Colors[0]}}><span className='title'>Controllers</span><span className='title_value'>{dashboard.controller}</span></div>
+                <div className='dashboard_card' style={{backgroundColor:Colors[1]}}><span className='title'>Firewalls</span><span className='title_value'>{dashboard.firewalls}</span></div>
+                <div className='dashboard_card' style={{backgroundColor:Colors[2]}}><span className='title'>Access Points</span><span className='title_value'>{dashboard.access_points}</span></div>
+                <div className='dashboard_card' style={{backgroundColor:Colors[3]}}><span className='title'>Internet</span><span className='title_value'>{dashboard.internet_accounts}</span></div>
+        </div>
+      <div className='dashboard_title'>
+        <h6>Vendors</h6>
+      </div>
+      <div className='vendors'>
+      <div className='vendors_container'>
+      <table>
+        <thead>
+            <tr>
+              <th>Vendor</th>
+              <th>Qty</th>
+            </tr>
+        </thead>
+          {sw_vendors.map(item => (
+            <tr>
+            <td>{item.vendor}</td>
+            <td>{item.count}</td>
+            </tr>
+          ))}
+          </table>
+      </div>
+      <div className='vendors_container'>
+      <table>
+      <thead>
+            <tr>
+              <th>Vendor</th>
+              <th>Qty</th>
+            </tr>
+        </thead>
+          {fw_vendors.map(item => (
+            <tr>
+            <td>{item.vendor}</td>
+            <td>{item.count}</td>
+            </tr>
+          ))}
+          </table>
+      </div>
+      <div className='vendors_container'>
+          <table>
+          <thead>
+            <tr>
+              <th>Vendor</th>
+              <th>Qty</th>
+            </tr>
+        </thead>
+          {controller_vendors.map(item => (
+            <tr>
+            <td>{item.vendor}</td>
+            <td>{item.count}</td>
+            </tr>
+          ))}
+          </table>
+      </div>
+      <div className='vendors_container'>
+      <table>
+      <thead>
+            <tr>
+              <th>Vendor</th>
+              <th>Qty</th>
+            </tr>
+        </thead>
+          {ap_vendors.map(item => (
+            <tr>
+            <td>{item.vendor}</td>
+            <td>{item.count}</td>
+            </tr>
+          ))}
+          </table>
+      </div>
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/SampleData/ColorList.js
@@ -0,0 +1,2 @@
+const Colors = ["#ccf8e3","#f8e9cc","#f0ccf8","#f8f5cc"]
+export default Colors;
