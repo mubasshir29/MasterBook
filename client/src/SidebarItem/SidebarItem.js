04e5fa490f65ec3488bcd91f835aa14572ca1b5b
mubasshir29 <mubasshir.f29>
2023-01-07 11:34:53 +0400

    after removing scrollbar width in sidebar

--- client/src/SidebarItem/SidebarItem.js
+++ client/src/SidebarItem/SidebarItem.js
@@ -15,7 +15,7 @@ function SidebarItem({item}) {
             { item.childrens && <i className="bi bi-caret-down-fill toggle-btn" onClick={()=>{setOpen(!open)}}></i>}
         </div>
         {item.childrens && <div className="sidebar-content">
-            { item.childrens.map(entry => (<div><Link to={entry.path}>{entry.title}</Link></div>)) }
+            { item.childrens.map(entry => (<div><Link to={entry.path}><i className={entry.icon}></i>{entry.title}</Link></div>)) }
             
         </div>}
     </div>
